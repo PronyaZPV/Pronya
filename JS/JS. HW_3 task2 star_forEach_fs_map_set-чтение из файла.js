88b77fe8e13119ff23eefc59708dc16269356709
PronyaZPV <pronyazpv@ya.ru>
2022-06-02 18:49:21 +0300

    JS_HW update

--- JS/JS. HW_3 task2 star_forEach_fs_map_set-чтение из файла.js
+++ JS/JS. HW_3 task2 star_forEach_fs_map_set-чтение из файла.js
@@ -5,7 +5,7 @@ const fs = require('fs')                    // загружаем модуль f
 let file = fs.readFileSync('JS. HW_3 task2.json')    // получаем данные из файла (в скобках относительный путь к файлу) в шестнадцатеричном виде
 let personFile = JSON.parse(file)           // переводим шестнадцатеричные данные в JSON
 
-function person (person) {
+/* function person (person) {
     person.forEach ((user,m) => {             // для каждого элемента user и его порядкового номера m из массива
         for (i = person.length - 1; i > m; i--) {      // вложенный цикл для встречного перебора элементов в массиве с конца
             if (user.username == person[i].username && user.email == person[i].email) {   // сравниваю 'username' и 'email', если они равны, то удаляю элемент, который ближе к концу списка
@@ -18,5 +18,14 @@ function person (person) {
         console.log(person[n].name, person[n].username)
     }
 }
+person(personFile) */
 
-person(personFile)
+
+// другое решение
+let strArr = personFile.map(item => JSON.stringify(item))   // все объекты-карточки превращаем в строки и запаковываем в массив
+let setArr = new Set(strArr)    // убрали все дубли в массиве
+let Arr = Array.from(setArr)     // set переделали в массив
+let resJSON = Arr.map(item => JSON.parse(item))  // массив превращае в JSON
+//let resJSON = Array.from(new Set(personFile.map(item => JSON.stringify(item)))).map(item => JSON.parse(item)) // то же самое в одну строку
+console.log(resJSON)
+
